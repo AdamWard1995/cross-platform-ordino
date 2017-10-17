391a3ae15826929ede25a15351877ef6bc27cadb
Adam Ward <adam.ward@carleton.ca>
2017-10-17 13:40:28 -0400

    Try to fix bash script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,7 @@
-cat coverage/lcov.info | coveralls
-ember build
-firebase deploy --token "$FIREBASE_TOKEN"
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
+  cat coverage/lcov.info | coveralls
+  ember build
+  firebase deploy --token "$FIREBASE_TOKEN"
+fi
