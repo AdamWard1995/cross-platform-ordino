94f9b9add119055e96cf8fff9e4c6bad062949c2
Adam Ward <adam.ward@carleton.ca>
2017-10-29 13:06:43 -0400

    Remove un-needed blueprint code

--- ember-electron/main.js
+++ ember-electron/main.js
@@ -1,97 +1,11 @@
 /* eslint-env node */
-const { app, BrowserWindow, /*protocol*/ } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const { dirname, join, resolve } = require('path');
-// const protocolServe = require('electron-protocol-serve');
 const connect = require('connect'); // connect server
 const serveStatic = require('serve-static'); // serve static files
 
 let mainWindow = null;
 let server = null;
-
-// Registering a protocol & schema to serve our Ember application
-// protocol.registerStandardSchemes(['http'], { secure: false });
-// protocolServe({
-//   cwd: join(__dirname || resolve(dirname('')), '..', 'ember'),
-//   app,
-//   protocol,
-//   name: 'http'
-// });
-
-// Uncomment the lines below to enable Electron's crash reporter
-// For more information, see http://electron.atom.io/docs/api/crash-reporter/
-// electron.crashReporter.start({
-//     productName: 'YourName',
-//     companyName: 'YourCompany',
-//     submitURL: 'https://your-domain.com/url-to-submit',
-//     autoSubmit: true
-// });
-
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
-
-function handleSquirrelEvent() {
-  if (process.argv.length === 1) {
-    return false;
-  }
-
-  const ChildProcess = require('child_process');
-  const path = require('path');
-
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.resolve(path.join(rootAtomFolder, 'app-1.0.0', 'Ordino.exe'));
-
-  const spawn = function(command, args) {
-    let spawnedProcess, error;
-
-    try {
-      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
-    } catch (error) {}
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function(args) {
-    return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      app.quit();
-      return true;
-  }
-}
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -108,9 +22,6 @@ app.on('ready', () => {
   mainWindow.maximize();
   mainWindow.show();
 
-  // If you want to open up dev tools programmatically, call
-  // mainWindow.openDevTools();
-
   if (!server) {
     server = connect()
       .use(serveStatic(join(__dirname || resolve(dirname('')), '..', 'ember'))).listen(4200);
@@ -118,26 +29,20 @@ app.on('ready', () => {
 
   const emberAppLocation = 'http://localhost:4200/';
 
-  // Load the ember application using our custom protocol/scheme
+  // Load the statically served ember application
   mainWindow.loadURL(emberAppLocation);
 
-  // If a loading operation goes wrong, we'll send Electron back to
-  // Ember App entry point
   mainWindow.webContents.on('did-fail-load', () => {
     mainWindow.loadURL(emberAppLocation);
   });
 
   mainWindow.webContents.on('crashed', () => {
-    console.log('Your Ember app (or other code) in the main window has crashed.');
-    console.log('This is a serious issue that needs to be handled and/or debugged.');
   });
 
   mainWindow.on('unresponsive', () => {
-    console.log('Your Ember app (or other code) has made the window unresponsive.');
   });
 
   mainWindow.on('responsive', () => {
-    console.log('The main window has become responsive again.');
   });
 
   mainWindow.on('closed', () => {
@@ -145,23 +50,5 @@ app.on('ready', () => {
   });
 });
 
-// Handle an unhandled error in the main thread
-//
-// Note that 'uncaughtException' is a crude mechanism for exception handling intended to
-// be used only as a last resort. The event should not be used as an equivalent to
-// "On Error Resume Next". Unhandled exceptions inherently mean that an application is in
-// an undefined state. Attempting to resume application code without properly recovering
-// from the exception can cause additional unforeseen and unpredictable issues.
-//
-// Attempting to resume normally after an uncaught exception can be similar to pulling out
-// of the power cord when upgrading a computer -- nine out of ten times nothing happens -
-// but the 10th time, the system becomes corrupted.
-//
-// The correct use of 'uncaughtException' is to perform synchronous cleanup of allocated
-// resources (e.g. file descriptors, handles, etc) before shutting down the process. It is
-// not safe to resume normal operation after 'uncaughtException'.
 process.on('uncaughtException', (err) => {
-  console.log('An exception in the main thread was not handled.');
-  console.log('This is a serious issue that needs to be handled and/or debugged.');
-  console.log(`Exception: ${err}`);
 });
