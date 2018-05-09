80dfa0f029d4ac612a19e44007493f641c5cbea4
Adam Ward <adam.ward@carleton.ca>
2018-05-09 15:52:44 -0400

    Add mobile device link

--- app/components/download-modal.js
+++ app/components/download-modal.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  computedClass: Ember.computed('isMobile', function() {
+    if (this.get('isMobile')) {
+      return 'is-mobile';
+    }
+    return 'is-desktop';
+  }),
   actions: {
     close () {
-      if (this.get('onClose')) {
+      if (!this.get('isMobile') && this.get('onClose')) {
         this.get('onClose')();
       }
     }
