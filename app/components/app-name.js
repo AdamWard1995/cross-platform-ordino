3783fde3fe7c6afac27574c92ca9890ac1fb3ae3
Adam Ward <adam.ward@carleton.ca>
2018-09-05 19:15:44 -0400

    Re-style website

--- app/components/app-name.js
+++ app/components/app-name.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  filteredItems: Ember.computed('backgroundTransparent', '', function() {
+    if (this.get('backgroundTransparent')) {
+      return 'background-transparent';
+    }
+  })
 });
